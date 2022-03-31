719a5ee0b1ece9258314282e9d07f2d0db807ef8
Al-Imran <alimran21feb@gmail.com>
2022-03-31 22:55:42 +0600

    added crypto cafe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Coins from './components/Coins/Coins';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Homepage from './components/Homepage/Homepage';
+import About from './components/About/About';
+import CoinDetails from './components/CoinDetails/CoinDetails';
+import BdAddress from './components/Contact/BdAddress';
+import UsAddress from './components/Contact/UsAddress';
+import NotFound from './components/NotFound/NotFound';
+
 
 const App = () => {
   return (
     <div>
-      <h1>Crypto Cafe Starter Template</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Homepage></Homepage>} />
+        <Route path='/coins' element={<Coins></Coins>}></Route>
+        <Route path='/coin-details/:id' element={<CoinDetails></CoinDetails>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}>
+
+          <Route path='bd-address' element={<BdAddress></BdAddress>}></Route>
+          <Route path='us-address' element={<UsAddress></UsAddress>}></Route>
+
+        </Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
+      
     </div>
   )
 }
